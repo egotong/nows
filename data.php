4eabe02957a6bcdf327b214939d07ffdbc0eb100
Flying-Alpaca <112460984+Flying-Alpaca@users.noreply.github.com>
2022-08-30 13:14:24 +0800

    Update data.php

--- data.php
+++ data.php
@@ -1,5 +1,5 @@
 <?php
-	$conn=mysql_connect("localhost","symhytwg_fun","wahaha2019"); //连接数据库地址、用户名、密码
-	mysql_query("set names 'utf8'"); //数据库编码
-	mysql_select_db("sy_nows"); //数据库名称
+	$conn = mysqli_connect("db_hostname","db_username","db_password"); //连接数据库地址、用户名、密码
+	//mysql_query("set names 'utf8'"); //数据库编码
+	mysqli_select_db($conn,"sy_nows"); //数据库名称
 ?>
